3c807804406022247bef923b87553f97074b1046
Alex Marcotte <alex.marcotte@gmail.com>
2017-08-07 10:43:41 -0400

    Implementing version command

--- commands/version.go
+++ commands/version.go
@@ -1,5 +1,27 @@
 package commands
 
+import (
+	"fmt"
+	"github.com/gnumast/td/todo"
+	"runtime"
+)
+
 const (
 	VERSION = "0.0.1"
 )
+
+type VersionCommand struct{}
+
+func (c *VersionCommand) Run(configuration *todo.Configuration) error {
+	fmt.Printf("td version %s %s/%s\n", VERSION, runtime.GOOS, runtime.GOARCH)
+
+	return nil
+}
+
+func (c *VersionCommand) Parse(configuration *todo.Configuration) error {
+	return nil
+}
+
+func (c *VersionCommand) Validate(configuration *todo.Configuration) error {
+	return nil
+}
